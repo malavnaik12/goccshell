3eb184abe9a29a681034be5b3a9952aca51f9dcf
Malav Naik <malavnaik12@gmail.com>
2024-02-04 18:06:33 -0500

    Code status following step 5

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	for {
+
+		// Display the Shell Input
+		fmt.Print("ccsh> ")
+
+		// Read User Input
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		parts := strings.Fields(input)
+		if input == "" {
+			continue
+		}
+		var command = parts[0]
+		var args = parts[1:]
+		switch command {
+		case "exit":
+			os.Exit(0)
+
+		case "cd":
+			var path string
+			var err error
+
+			if len(args) > 0 {
+				path = args[0]
+			} else {
+				path, _ = os.UserHomeDir()
+			}
+			err = os.Chdir(path)
+			if err != nil {
+				fmt.Printf("%v\n", err)
+			}
+		case "pwd":
+			dir, _ := os.Getwd()
+			fmt.Println(dir)
+
+		default:
+			cmd := exec.Command(command, args...)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
